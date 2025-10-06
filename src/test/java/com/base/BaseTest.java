6f677eac1fed2ad44e448aaf182aa8d29077b65d
Ranum Khan <ranumkhan123@gmail.com>
2025-10-06 17:28:16 +0500

    testng.xml updated

--- src/test/java/com/base/BaseTest.java
+++ src/test/java/com/base/BaseTest.java
@@ -3,8 +3,13 @@ package com.base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import pages.*;
 
 public class BaseTest {
@@ -16,14 +21,28 @@ public class BaseTest {
     protected addProductsToCartPage addProductsToCart;
 
     @BeforeTest
-    public void setUp() {
-        // Browser setup
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--incognito");
-        options.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(options);
+    @Parameters("browser")
+    public void setUp(String browser) {
 
-        // Page objects initialize
+        if (browser.equalsIgnoreCase("chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--incognito");
+            options.addArguments("--remote-allow-origins=*");
+            driver = new ChromeDriver(options);
+
+        } else if (browser.equalsIgnoreCase("edge")) {
+            EdgeOptions options = new EdgeOptions();
+            driver = new EdgeDriver(options);
+
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            FirefoxOptions options = new FirefoxOptions();
+            driver = new FirefoxDriver(options);
+
+        } else {
+            throw new IllegalArgumentException("Browser not supported: " + browser);
+        }
+
+        // ✅ Initialize all page objects AFTER driver is ready
         loginPage = new LoginPage(driver);
         addtocart = new AddtoCartPage(driver);
         checkout = new CheckoutPage(driver);
@@ -31,9 +50,10 @@ public class BaseTest {
         addProductsToCart = new addProductsToCartPage(driver);
     }
 
+
     @AfterTest
     public void tearDown() {
-        if(driver != null) {
+        if (driver != null) {
             driver.quit();
         }
     }
